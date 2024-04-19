9f9b634cb04ee88ecf260f1be9a6a773da5bcdcd
Maikol Rodriguez <maikolrodriguezm@gmail.com>
2024-04-19 01:42:52 -0400

    Me gusta

---
+++ script.js
@@ -0,0 +1,23 @@
+var count1 = 9;
+var aumentarlike1 = document.querySelector("#numeroLikes1");
+
+function subirLike1() {
+    count1++;
+    aumentarlike1.innerHTML = count1 + "like(s)"
+}
+
+var count2 = 12;
+var aumentarlike2 = document.querySelector("#numeroLikes2");
+
+function subirLike2() {
+  count2++;
+  aumentarlike2.innerHTML = count2 + "like(s)";
+}
+
+var count3 = 9;
+var aumentarlike3 = document.querySelector("#numeroLikes3");
+
+function subirLike3() {
+  count3++;
+  aumentarlike3.innerHTML = count3 + "like(s)";
+}
